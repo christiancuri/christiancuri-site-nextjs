a1372052670dd747c1b3d733ff2435e264ce5b0a
Christian Curi <c.christiancuri@gmail.com>
2021-04-04 02:10:56 -0300

    fix: add viewport on all pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,29 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import '../styles/globals.css';
 
+import Head from 'next/head';
+
 type Props = {
   Component?: any;
   pageProps?: any;
 };
 
-function MyApp({ Component, pageProps }: Props) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }: Props): JSX.Element {
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.10.1/devicon.min.css"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,22 +2,6 @@ import React from 'react';
 
 import Home from '@/components/Home';
 
-import Head from 'next/head';
-
-const IndexPage = (): JSX.Element => (
-  <div>
-    <Head>
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
-      />
-      <link
-        rel="stylesheet"
-        href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.10.1/devicon.min.css"
-      />
-    </Head>
-    <Home />
-  </div>
-);
+const IndexPage = (): JSX.Element => <Home />;
 
 export default IndexPage;
