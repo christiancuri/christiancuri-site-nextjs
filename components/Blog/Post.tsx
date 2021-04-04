be503b05cfeab42ff705c9f8af812db37e58ef00
Christian Curi <c.christiancuri@gmail.com>
2021-04-03 23:43:56 -0300

    feat: add sitemap

--- components/Blog/Post.tsx
+++ components/Blog/Post.tsx
@@ -11,10 +11,11 @@ type Props = {
 };
 
 export default function Post({ post }: Props): ReactElement {
+  const postURL = `/blog/post/${post.uri}`;
   return (
-    <Link href={`/blog/post/${post.uri}`} as={`/blog/post/${post.uri}`}>
+    <Link href={postURL} as={postURL}>
       <div className="col-md-6 col-xl-4 mb-5">
-        <a className="card post-preview lift h-100" href="#!">
+        <a className="card post-preview lift h-100" href={postURL}>
           <img alt="" className="card-img-top" src={post.picture} />
           <div className="card-body">
             <h5 className="card-title">{post.title}</h5>
