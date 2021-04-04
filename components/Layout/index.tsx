5e91d7e28177c2dcacdee77c55ed97fce653f260
Christian Curi <c.christiancuri@gmail.com>
2021-04-03 23:09:48 -0300

    feat: add viewport

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -3,6 +3,7 @@ import React, { ReactNode } from 'react';
 import Footer from '@/components/Footer';
 import Navbar from '@/components/Navbar';
 
+import Head from 'next/head';
 import styled from 'styled-components';
 
 const Content = styled.div`
@@ -13,11 +14,19 @@ const Content = styled.div`
 type Props = {
   children: ReactNode;
   footerDark?: boolean;
+  title?: string;
 };
 
-const Home = ({ children, footerDark }: Props): JSX.Element => {
+const Home = ({ children, footerDark, title }: Props): JSX.Element => {
   return (
     <>
+      <Head>
+        <title>
+          {title
+            ? `${title} | ChristianCuri.dev`
+            : 'My personal website | ChristianCuri.dev'}
+        </title>
+      </Head>
       <div id="layoutDefault">
         <Content>
           <main>
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,10 @@ import Head from 'next/head';
 const IndexPage = (): JSX.Element => (
   <div>
     <Head>
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
+      />
       <link
         rel="stylesheet"
         href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.10.1/devicon.min.css"
